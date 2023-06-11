67c3f63f2abd518e7797e56f748e501292c51bb1
rokiboxofficial <rokiboxofficial@gmail.com>
2023-06-11 17:08:50 +0300

    feat: add base structure

---
+++ TicTacToe.Web/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace TicTacToe.Web.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddJwtBearerAuthentication(this IServiceCollection serviceCollection, string secretKey)
+    {
+        serviceCollection.AddAuthentication(options =>
+        {
+            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        }).AddJwtBearer(options =>
+        {
+            options.TokenValidationParameters.IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey));
+            options.TokenValidationParameters.ValidateIssuerSigningKey = true;
+            options.TokenValidationParameters.ValidateActor = false;
+            options.TokenValidationParameters.ValidateAudience = false;
+            options.TokenValidationParameters.ValidateIssuer = false;
+            options.TokenValidationParameters.ValidateLifetime = false;
+            options.TokenValidationParameters.ValidateTokenReplay = false; ;
+        
+            options.Events = new JwtBearerEvents
+            {
+                OnMessageReceived = context =>
+                {
+                    var accessToken = context.Request.Query["access_token"];
+        
+                    var path = context.HttpContext.Request.Path;
+                    if (!string.IsNullOrEmpty(accessToken) &&
+                        (path.StartsWithSegments("/hubs")))
+                    {
+                        context.Token = accessToken;
+                    }
+                    return Task.CompletedTask;
+                }
+            };
+        });
+
+        return serviceCollection;
+    }
+}
--- TicTacToe.Web/Program.cs
+++ TicTacToe.Web/Program.cs
@@ -1,3 +1,5 @@
+using TicTacToe.Web.Extensions;
+
 namespace TicTacToe.Web;
 
 internal class Program
@@ -5,8 +7,24 @@ internal class Program
     private static void Main()
     {
         var builder = WebApplication.CreateBuilder();
-        var app = builder.Build();
 
+        builder.Configuration.AddJsonFile("auth_options.json");
+        var jwtSecretKey = builder.Configuration["jwtSecretKey"]!.ToString();
+        builder.Services.AddJwtBearerAuthentication(jwtSecretKey!);
+        builder.Services.AddSignalR();
+        builder.Services.AddCors();
+
+        var app = builder.Build();
+        app.UseCors(options => options
+                   .AllowAnyHeader()
+                   .AllowAnyMethod()
+                   .SetIsOriginAllowed((host) => true)
+                   .AllowCredentials());
+
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        app.UseCors();
         app.Run();
     }
 }
